833923ce240a9f2285b5bc5c7c6a6fe74b73504b
El mehdi Salama <mehdisalama61@gmail.com>
2026-03-08 13:03:00 +0000

    mapper function built

---
+++ mapper.go
@@ -0,0 +1,24 @@
+package main
+
+import(
+	"strings"
+)
+
+type KeyValue struct{
+	Key string
+	Value string
+}
+
+func Mapper(input_string string)[]KeyValue{
+	var my_list []KeyValue
+	words := strings.Fields(input_string)
+	for _, word := range words{
+		new_object := KeyValue{
+			Key : word,
+			Value : "1",
+		}
+		my_list = append(my_list, new_object)
+	}
+	return my_list
+}
+
